c103dfdd973461917e16b44029d5e1bc5231fd34
Kang Noir <simaul.noir@gmail.com>
2023-06-16 02:58:44 +0000

    add authUser action creator

--- src/states/authUser/action.js
+++ src/states/authUser/action.js
@@ -1,3 +1,49 @@
 /**
  * @TODO: Define all the actions (creator) for the authUser state
  */
+
+import api from '../../utils/api';
+
+export const ActionType = {
+  SET_AUTH_USER: 'SET_AUTH_USER',
+  UNSET_AUTH_USER: 'UNSET_AUTH_USER',
+};
+
+export function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: {
+      authUser,
+    },
+  };
+}
+
+export function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+    payload: {
+      authUser: null,
+    },
+  };
+}
+
+export function asyncSetAuthUser({ id, password }) {
+  return async (dispatch) => {
+    try {
+      const token = await api.login({ id, password });
+      api.putAccessToken(token);
+      const authUser = await api.getOwnProfile();
+
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export function asyncUnsetAuthUser() {
+  return (dispatch) => {
+    dispatch(unsetAuthUserActionCreator());
+    api.putAccessToken('');
+  };
+}
