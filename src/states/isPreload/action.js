1bdc8476293ebbcb7767731b6a5363ff68dc5a60
Kang Noir <simaul.noir@gmail.com>
2023-06-16 03:14:29 +0000

    add isPreload action creator

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,35 @@
 /**
  * @TODO: Define all the actions (creator) for the isPreLoad state
  */
+
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+export const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+export function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreload,
+    },
+  };
+}
+
+export function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      // preload process
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // fallback process
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // end preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
