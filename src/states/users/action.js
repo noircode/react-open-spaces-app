a794dc5b30dfcf77460947ea2cf486afb9b5645a
Kang Noir <simaul.noir@gmail.com>
2023-06-16 03:30:37 +0000

    add users action creator

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,16 @@
 /**
  * @TODO: Define all the actions (creator) for the users state
  */
+
+export const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS',
+};
+
+export function receiveUsersActionCreator(users) {
+  return {
+    type: ActionType.RECEIVE_USERS,
+    payload: {
+      users,
+    },
+  };
+}
