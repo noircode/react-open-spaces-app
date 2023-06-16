5c5a1296f6a1d8760499121d00c3858a264cc469
Kang Noir <simaul.noir@gmail.com>
2023-06-16 03:43:25 +0000

    add talks action creator

--- src/states/talks/action.js
+++ src/states/talks/action.js
@@ -1,3 +1,64 @@
 /**
  * @TODO: Define all the actions (creator) for the talks state
  */
+
+import api from '../../utils/api';
+
+export const ActionType = {
+  RECEIVE_TALKS: 'RECEIVE_TALKS',
+  ADD_TALK: 'ADD_TALK',
+  TOGGLE_LIKE_TALK: 'TOGGLE_LIKE_TALK',
+};
+
+export function receiveTalksActionCreator(talks) {
+  return {
+    type: ActionType.RECEIVE_TALKS,
+    payload: {
+      talks,
+    },
+  };
+}
+
+export function addTalkActionCreator(talk) {
+  return {
+    type: ActionType.ADD_TALK,
+    payload: {
+      talk,
+    },
+  };
+}
+
+export function toggleLikeTalkActionCreator({ talkId, userId }) {
+  return {
+    type: ActionType.TOGGLE_LIKE_TALK,
+    payload: {
+      talkId,
+      userId,
+    },
+  };
+}
+
+export function asyncAddTalk({ text, replyTo = '' }) {
+  return async (dispatch) => {
+    try {
+      const talk = await api.createTalk({ text, replyTo });
+      dispatch(addTalkActionCreator(talk));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export function asyncToggleLikeTalk(talkId) {
+  return async (dispatch, getState) => {
+    const { authUser } = getState();
+    dispatch(toggleLikeTalkActionCreator({ talkId, userId: authUser.id }));
+
+    try {
+      await api.toggleLikeTalk(talkId);
+    } catch (error) {
+      alert(error.message);
+      dispatch(toggleLikeTalkActionCreator({ talkId, userId: authUser.id }));
+    }
+  };
+}
